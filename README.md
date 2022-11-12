058105937035e4eb96d39a03e37e06b604c97965
oreomaker <zh002919@outlook.com>
2022-11-12 17:11:34 +0800

    style: config clang format

---
+++ README.md
@@ -0,0 +1,10 @@
+# The Rail Ticket System
+
+this is a simple system to buy tickets for a train
+
+## How to run
+
+requirements:
+
+[drogon framework](https://github.com/drogonframework/drogon/wiki/CHN-02-%E5%AE%89%E8%A3%85)
+sqlite3
--- controllers/TestCtrl.cc
+++ controllers/TestCtrl.cc
@@ -5,7 +5,8 @@ void TestCtrl::asyncHandleHttpRequest(const HttpRequestPtr &req,
     //write your application logic here
     auto resp=HttpResponse::newHttpResponse();
     resp->setStatusCode(k200OK);
-    resp->setContentTypeCode(CT_TEXT_HTML);
-    resp->setBody("Hello World!");
+    // resp->setContentTypeCode(CT_TEXT_HTML);
+    resp->setContentTypeCode(CT_APPLICATION_JSON);
+    resp->setBody("{\"status\":\"ok\"}");
     callback(resp);
 }
